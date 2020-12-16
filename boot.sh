bd23facb63f5fff12d191aaa6f7fe7ddb256e8ad
Vladimir Bogretsov <v.bogretsov@itransition.com>
2020-12-16 10:45:21 +0300

    Updated versions

--- boot.sh
+++ boot.sh
@@ -97,4 +97,4 @@ if [ -n "${metadata}" ] ; then
     echo "${metadata}" > $state/metadata.json
 fi
 
-exec linuxkit run ${KUBE_RUN_ARGS} -networking ${KUBE_NETWORKING} -cpus ${KUBE_VCPUS} -mem ${KUBE_MEM} -state "${state}" -disk size=${KUBE_DISK} -data-file $state/metadata.json -iso ${uefi} "${img}${suffix}"
+exec linuxkit run ${KUBE_RUN_ARGS} -publish 3022:22/tcp -networking ${KUBE_NETWORKING} -cpus ${KUBE_VCPUS} -mem ${KUBE_MEM} -state "${state}" -disk size=${KUBE_DISK} -data-file $state/metadata.json -iso ${uefi} "${img}${suffix}"
